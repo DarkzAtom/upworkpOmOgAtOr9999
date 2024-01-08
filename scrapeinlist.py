0876d57faf0ef35bb413468d12448fbb46c39870
DarkzAtom <rekrut.fedosenko@gmail.com>
2024-01-08 11:58:49 +0100

    added user-agent to chrome-options selenium

--- scrapeinlist.py
+++ scrapeinlist.py
@@ -10,7 +10,8 @@ import sys
 def scrapeitem(url):
     # url = "https://www.upwork.com/jobs/Coin-market-cap-span-class-highlight-scraping-span_~01a993c78a79304acf/"
     options = ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     # options.add_argument('--disable-gpu')
     # options.add_argument("--no-sandbox")
     # options.add_argument("--disable-dev-shm-usage")
--- scrapeinsite.py
+++ scrapeinsite.py
@@ -19,7 +19,8 @@ def scrapesite():
     url = "https://www.upwork.com/nx/search/jobs?client_hires=1-9,10-&payment_verified=1&q=scraping&sort=recency"
 
     options = ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     # options.add_argument('--disable-gpu')
     # options.add_argument("--no-sandbox")
     # options.add_argument("--disable-dev-shm-usage")
